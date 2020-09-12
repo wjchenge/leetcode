4a12d1a37d6c3f6bbe946ac57dd6c36dd7986950
wjchenge <wjchenge@qq.com>
2020-09-12 16:38:36 +0800

    9月12日每日一题（下次：9月13日)

---
+++ src/wjchenge/one/LeetCode637.java
@@ -0,0 +1,47 @@
+package wjchenge.one;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 637. 二叉树的层平均值
+ * https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/
+ * @author wjchenge
+ */
+public class LeetCode637 {
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            long sum = 0;
+            for (int i = 0; i < size; ++i) {
+                TreeNode node = q.poll();
+                sum += node.val;
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+            res.add((double) sum / size);
+        }
+        return res;
+    }
+
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
