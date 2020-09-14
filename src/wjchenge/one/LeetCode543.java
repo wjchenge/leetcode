cc4daa193d67249f4821bada9e5e26ed101efeeb
wjchenge <wjchenge@qq.com>
2020-09-14 22:27:14 +0800

    二叉树简单第一次（下次：9月15日)

---
+++ src/wjchenge/one/LeetCode543.java
@@ -0,0 +1,37 @@
+package wjchenge.one;
+
+/**
+ * 543. 二叉树的直径
+ * https://leetcode-cn.com/problems/diameter-of-binary-tree/
+ * @author wjchenge
+ */
+public class LeetCode543 {
+
+    private int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int l = dfs(root.left);
+        int r = dfs(root.right);
+        max = Math.max(max, l + r);
+        return Math.max(l, r) + 1;
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
