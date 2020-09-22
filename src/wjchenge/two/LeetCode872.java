5a33e1f32649436fc9133d016c88d73350221162
wangjian <jian.wang@webstudio.com.cn>
2020-09-22 17:44:22 +0800

    二叉树简单第二次（下次：9月29日)

---
+++ src/wjchenge/two/LeetCode872.java
@@ -0,0 +1,35 @@
+package wjchenge.two;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 872. 叶子相似的树
+ * https://leetcode-cn.com/problems/leaf-similar-trees/
+ * @author wjchenge
+ */
+public class LeetCode872 {
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        this.preOrder(root1, l1);
+        this.preOrder(root2, l2);
+        return l1.equals(l2);
+    }
+
+    private void preOrder(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        if (root.left == null && root.right == null) list.add(root.val);
+        preOrder(root.left, list);
+        preOrder(root.right, list);
+    }
+
+
+    private class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode(int x) { val = x; }
+     }
+}
