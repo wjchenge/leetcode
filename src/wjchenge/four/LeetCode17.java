32cf409bd89e1ce7f7be2f4a8429b28b4813e4f0
wangjian <jian.wang@webstudio.com.cn>
2020-10-15 15:48:43 +0800

    算法训练营-第8课第四次

---
+++ src/wjchenge/four/LeetCode17.java
@@ -0,0 +1,51 @@
+package wjchenge.four;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 17. 电话号码的字母组合
+ * https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+ * @author wjchenge
+ */
+public class LeetCode17 {
+
+    private static final Map<Character, char[]> map = new HashMap<>();
+
+    static {
+        map.put('2', new char[]{'a', 'b', 'c'});
+        map.put('3', new char[]{'d', 'e', 'f'});
+        map.put('4', new char[]{'g', 'h', 'i'});
+        map.put('5', new char[]{'j', 'k', 'l'});
+        map.put('6', new char[]{'m', 'n', 'o'});
+        map.put('7', new char[]{'p', 'q', 'r', 's'});
+        map.put('8', new char[]{'t', 'u', 'v'});
+        map.put('9', new char[]{'w', 'x', 'y', 'z'});
+    }
+
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+        if (digits == null || digits.length() == 0) return res;
+        this.letterCombinations(digits.toCharArray(), 0, res, new StringBuilder());
+
+        return res;
+    }
+
+    private void letterCombinations(char[] chars, int i, List<String> res, StringBuilder str) {
+        if (i == chars.length) {
+            res.add(str.toString());
+            return;
+        }
+        char[] letter = map.get(chars[i]);
+        for (int j = 0; j < letter.length; j++) {
+            str.append(letter[j]);
+            this.letterCombinations(chars, i + 1, res, str);
+            str.delete(str.length() - 1, str.length());
+        }
+
+    }
+
+
+}
